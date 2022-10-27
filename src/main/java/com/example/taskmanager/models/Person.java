8046179e5fb90a98ade6ea86086b6848ffbfad4f
Kirill Nikolaev <kirill.nikolaev.an@gmail.com>
2022-10-28 00:59:15 +0300

    create Person table and entity

---
+++ src/main/java/com/example/taskmanager/models/Person.java
@@ -0,0 +1,36 @@
+package com.example.taskmanager.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "person")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "role")
+    @Enumerated
+    private Role role;
+}
---
+++ src/main/java/com/example/taskmanager/models/Role.java
@@ -0,0 +1,9 @@
+package com.example.taskmanager.models;
+
+public enum Role {
+    ROLE_GUEST,
+    ROLE_DEVELOPER,
+    ROLE_MANAGER,
+    ROLE_DIRECTOR,
+    ROLE_ADMIN
+}
